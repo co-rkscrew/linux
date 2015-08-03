744e644553d5918cabe7f09632366c30fa0ed41c
Joao Rodrigues <jlrodrigues87@gmail.com>
2015-08-03 14:16:41 +0100

    issue #2 - changed control description; refined deb_builder.sh

--- deb_builder.sh
+++ deb_builder.sh
@@ -8,13 +8,15 @@ DEB_DIR="/vagrant/deb"
 mkdir -p $DEB_DIR
 
 function set_script_perms {
-	find "$PKG_DIR" -type f -name "$1" | xargs chmod 775
+	find "$PKG_DIR" -type f -name "$1" | xargs -I {} chmod 775 {}
 }
 
 set_script_perms "preinst"
+set_script_perms "postinst"
+set_script_perms "prerm"
 set_script_perms "postrm"
 
 # each package subfolder represent a deb package to be build
-find "$PKG_DIR" -maxdepth 1 ! -path "$PKG_DIR" -type d | while read line; do
+find "$PKG_DIR" -maxdepth 1 ! -path "$PKG_DIR" -type d | sort | while read line; do
 		dpkg-deb --build $line $DEB_DIR
 done
